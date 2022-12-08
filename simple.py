ddecc7d1e9770b8425a78cb8fd00cb5b1481c1c0
ratuerror08 <108736345+ratuerror08@users.noreply.github.com>
2022-12-08 13:23:23 +0700

    Update simple.py

--- simple.py
+++ simple.py
@@ -132,7 +132,7 @@ def menu():
 			uas = ("Mozilla/5.0 (Linux; Android 10; Redmi Note 9 Pro Build/QKQ1.191215.002; wv) AppleWebKit/537.36 (KHTML, like Gecko) Version/4.0 Chrome/91.0.4472.77 Mobile Safari/537.36 [FB_IAB/FB4A;FBAV/325.0.0.36.170;]")
 			open('ugent.txt','r').write(uas)
 		else:
-			open('ugent.txt','r').write(us)
+			open('ugent.txt','w').write(us)
 	elif romz in ['0']:
 		exit()
 	else:
